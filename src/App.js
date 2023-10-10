17645cb28e4d6d4305456ef377240cf8870563bc
Walid Younis <walid.younis.25@gmail.com>
2023-10-10 11:30:00 -0500

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
-
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Body />
       <Footer />
     </div>
